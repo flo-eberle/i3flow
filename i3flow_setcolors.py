da89b08d61266dd1d38f4c02bfb4bbeb991b86c3
Flo Eberle <info@florianeberle.com>
2019-11-07 19:45:44 +0100

    set up vim colors

--- i3flow_setcolors.py
+++ i3flow_setcolors.py
@@ -1,36 +1,43 @@
 #!/usr/bin/env python3
 
-# TODO: formatter for polybar
-# TODO: formatter for termite
-# TODO: formatter for vim
 # TODO: formatter for rofi
-# TODO: injector for i3
-# TODO: injector for polybar
-# TODO: injector for termite
-# TODO: injector for vim
-# TODO: injector for rofi
+# TODO: vim: colors_name
+# TODO: merge xrdb
+# TODO: reload i3
+# TODO: reload polybar
+# TODO: reload termite
+# TODO: reload vim
 
 import yaml
 import utils.formatters
 
 SCHEMEPATH = '/home/flo/i3flow/schemes/tonerlow.yaml'
 
-XRESOURCES = '/home/flo/i3flow/testing/.Xresources'
-I3CONFIG = '/home/flo/i3flow/testing/i3.config'
+CONFIG_FILES = {
+    'xresources': '/home/flo/i3flow/testing/.Xresources',
+    'termite': '/home/flo/i3flow/testing/termite.config',
+    'vim': '/home/flo/i3flow/testing/tonerlow.vim',
+}
 
 with open(SCHEMEPATH, 'r') as f:
     scheme = yaml.full_load(f) 
     # print(scheme)
 
-with open(I3CONFIG, 'r') as config:
-    config_lines = config.readlines()
+def write_configs(application, config_path):
+    with open(config_path, 'r') as config:
+        config_lines = config.readlines()
 
-formatted_colors = utils.formatters.formati3(scheme)
+    formatted_colors = utils.formatters.formatter(application, scheme)
 
-color_open, color_close = utils.formatters.find_color_indices(config_lines)
+    color_open, color_close = utils.formatters.find_color_indices(config_lines)
 
-# overwrite part between color tags
-config_lines[color_open:color_close] = formatted_colors
+    # overwrite part between color tags
+    config_lines[color_open:color_close] = formatted_colors
 
-for line in config_lines:
-    print(line, end='')
+    with open(config_path, 'w') as output:
+        output.writelines(config_lines)
+
+for key, value in CONFIG_FILES.items():
+    print(f'Writing config for "{key}" to {value}')
+    write_configs(key, value)
+    print('Done!')
--- utils/formatters.py
+++ utils/formatters.py
@@ -12,36 +12,82 @@ def find_color_indices(lines):
         i += 1
     return (color_open, color_close)
 
-# format for .Xresources
-# #define base00 #xxxxxx
-def formatXresources(scheme):
-    colors=['\n']
-    comment = '!'
-    for key, value in scheme.items():
-        if 'base' in key:
-            if 'base00' in key:
-                colors.append('\n')
-            line = f'#define {key} #{value}\n'
-        else:
-            line = f'{comment} {key}: {value}\n'
-        colors.append(line)
 
-    colors.append('\n')
+def formatter(config_type, scheme):
+    if config_type == 'xresources':
+        # format for .Xresources
+        # #define base00 #xxxxxx
+        colors=['\n']
+        comment = '!'
+        for key, value in scheme.items():
+            if 'base' in key:
+                if 'base00' in key:
+                    colors.append('\n')
+                line = f'#define {key} #{value}\n'
+            else:
+                line = f'{comment} {key}: {value}\n'
+            colors.append(line)
+
+        colors.append('\n')
+
+    elif config_type == 'termite':
+        # format for termite
+        colors = ['\n']
+        comment = '#'
+        # color mappings
+        termite_colors = {
+            'foreground': 'base05',
+            'foreground_bold': 'base06',
+            'cursor': 'base06',
+            'cursor_foreground': 'base00',
+            'background': 'base00',
+            'color0': 'base00',
+            'color8': 'base03',
+            'color7': 'base05',
+            'color15': 'base07',
+            'color1': 'base09',
+            'color9': 'base09',
+            'color2': 'base0B',
+            'color10': 'base0B',
+            'color3': 'base0A',
+            'color11': 'base0A',
+            'color4': 'base0D',
+            'color12': 'base0D',
+            'color5': 'base0E',
+            'color13': 'base0E',
+            'color6': 'base0C',
+            'color14': 'base0C',
+            'color16': 'base07',
+            'color17': 'base0F',
+            'color18': 'base01',
+            'color19': 'base02',
+            'color20': 'base04',
+            'color21': 'base06',
+            }
+        
+        if scheme['scheme']:
+            colors.append(f'{comment} scheme: {scheme["scheme"]}\n')
+        if scheme['author']:
+            colors.append(f'{comment} author: {scheme["author"]}\n\n')
+
+        for key, value in termite_colors.items():
+            colors.append(f'{key} = #{scheme[value]}\n')
+        colors.append('\n')
+
+    elif config_type == 'vim':
+        # format for vim
+        # let s:base00 = "xxxxxx"
+        colors = ['\n']
+        comment = '"'
+        for key, value in scheme.items():
+            if 'base' in key:
+                if 'base00' in key:
+                    colors.append('\n')
+                line = (f'let s:{key} = "{value}"\n')
+            else:
+                line = (f'{comment} {key}: {value}\n')
+            colors.append(line)
+        colors.append('\n')
+
     return colors
 
-# format for i3 config
-# set $base00 #xxxxxx
-def formati3(scheme):
-    colors=['\n']
-    comment = '#'
-    for key, value in scheme.items():
-        if 'base' in key:
-            if 'base00' in key:
-                colors.append('\n')
-            line = f'set ${key} #{value}\n'
-        else:
-            line = f'{comment} {key}: {value}\n'
-        colors.append(line)
-
-    colors.append('\n')
-    return colors
